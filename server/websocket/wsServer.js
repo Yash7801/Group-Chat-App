8b830111067760fe66445c79fe847c0acade14f2
Yash7801 <yashfine0099@gmail.com>
2026-04-06 11:40:00 +0530

    fix hyper-express websocket integrations-1

--- server/websocket/wsServer.js
+++ server/websocket/wsServer.js
@@ -1,23 +1,20 @@
 ﻿module.exports = function startWS(app) {
-  app.ws("/", {
-    open: (ws) => {
-      console.log("Client connected");
+  app.ws("/", (ws, req) => {
+    console.log("Client connected");
 
-      ws.on("message", (msg) => {
-        try {
-          const data = JSON.parse(msg.toString());
-          console.log("Received:", data);
+    ws.on("message", (msg) => {
+      try {
+        const data = JSON.parse(msg.toString());
+        console.log("Received:", data);
+      } catch (err) {
+        console.error("Invalid message");
+      }
+    });
 
-        } catch (err) {
-          console.error("Invalid message");
-        }
-      });
-
-      ws.on("close", () => {
-        console.log("Client disconnected");
-      });
-    }
+    ws.on("close", () => {
+      console.log("Client disconnected");
+    });
   });
 
-  console.log("WebSocket ready on /");
+  console.log("WebSocket ready 🚀");
 };
