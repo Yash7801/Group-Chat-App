d5d14207fc7666f30f04999d77874d550f29ab18
Yash7801 <yashfine0099@gmail.com>
2026-04-06 11:57:15 +0530

    fix hyper-express websocket integrations-3

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,10 +1,20 @@
-﻿import { useState } from "react";
+﻿import { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar.jsx";
 import ChatWindow from "../components/ChatWindow.jsx";
+import { onSocketMessage } from "../services/socket.js";
 
 export default function Chat({ userId, initialOnlineUsers = [] }) {
   const [onlineUsers, setOnlineUsers] = useState(initialOnlineUsers);
   const [activeChannel, setActiveChannel] = useState("general");
+
+  useEffect(() => {
+    const unsub = onSocketMessage((data) => {
+      if (data.type === "room_users") {
+        setOnlineUsers(data.users);
+      }
+    });
+    return () => unsub();
+  }, []);
 
   return (
     <div className="flex h-screen bg-[#0f1419] text-white">
--- server/websocket/handlers/roomHandler.js
+++ server/websocket/handlers/roomHandler.js
@@ -1,4 +1,15 @@
-const { joinRoom } = require("../rooms");
+const { joinRoom, getRoomUsers } = require("../rooms");
+const { getClient } = require("../connections");
+
+function broadcastToRoom(roomId, payload) {
+  const users = getRoomUsers(roomId);
+  users.forEach((username) => {
+    const client = getClient(username);
+    if (client) {
+      client.send(JSON.stringify(payload));
+    }
+  });
+}
 
 function handleRoomEvent(ws, data) {
   const { type, roomId } = data;
@@ -6,6 +17,13 @@ function handleRoomEvent(ws, data) {
   if (type === "join_room") {
     joinRoom(roomId, ws.username);
     console.log(`${ws.username} joined ${roomId}`);
+
+    const users = Array.from(getRoomUsers(roomId));
+    broadcastToRoom(roomId, {
+      type: "room_users",
+      roomId,
+      users,
+    });
   }
 }
 
